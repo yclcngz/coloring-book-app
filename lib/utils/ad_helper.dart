7f783888a82ff5b945c527b9efc8c8ac726a6d2b
Yucel <yclcngz07@gmail.com>
2026-09-07 20:17:47 +0300

    Fix: Remove dart:io for Web compatibility

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -1,22 +1,22 @@
-import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdHelper {
   static String get bannerAdUnitId {
-    if (Platform.isAndroid) {
+    if (defaultTargetPlatform == TargetPlatform.android) {
       return 'ca-app-pub-3940256099942544/6300978111'; // Test ID
-    } else if (Platform.isIOS) {
+    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
       return 'ca-app-pub-3940256099942544/2934735716'; // Test ID
     }
-    throw UnsupportedError('Unsupported platform');
+    return '';
   }
 
   static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
+    if (defaultTargetPlatform == TargetPlatform.android) {
       return 'ca-app-pub-3940256099942544/1033173712'; // Test ID
-    } else if (Platform.isIOS) {
+    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
       return 'ca-app-pub-3940256099942544/4411468910'; // Test ID
     }
-    throw UnsupportedError('Unsupported platform');
+    return '';
   }
 }
